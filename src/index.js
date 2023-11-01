afd69ccd152488ef1e9b0254c19f5fb2157ae91a
TheWildHustle <145122833+TheWildHustle@users.noreply.github.com>
2023-11-01 17:16:28 -0400

    Update index.js
    Added nostrLogin function

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+const nostrLogin= async () => {
+    const publicKey=  await window.nostr.getPublicKey();
+        console.log(publicKey);
+
 createRoot(document.getElementById('root')).render(<App/>)
